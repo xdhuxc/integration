0df5da11ae7484e233c4def82334c43b15417412
xdhuxc <xdhuxc@163.com>
2018-06-29 08:28:34 +0800

    增加 kubernetes 工具类。

---
+++ src/main/java/com/xdhuxc/kubernetes/KubernetesUtil.java
@@ -0,0 +1,12 @@
+package com.xdhuxc.kubernetes;
+
+/**
+ * @author xdhuxc
+ * @date 2018-06-29
+ **/
+
+public class KubernetesUtil {
+    //1、使用 Spring Template 调用 kubernetes REST API
+    //2、使用 fabric 8 来模板。
+
+}
